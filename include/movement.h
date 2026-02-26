9c692d24aa8360e971fee577bacc55ecce92411b
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-02-26 18:57:30 +0000

    created new header file and cpp

---
+++ include/movement.h
@@ -0,0 +1,15 @@
+// h is for header file. This is where we declare our classes and functions, but we don't define them here. We will define them in the .cpp file.
+
+#include <bn_sprite_ptr.h>
+#include <bn_fixed.h>
+
+/**
+ * Moves sprite up/down/left/right based on the d-pad being held.
+ * 
+ * Dimensions are moved independently, meaning that diagonal motion is faster than moving in one
+ * dimension.
+ * 
+ * @param sprite the sprite to move
+ * @param speed the speed at which to move the sprite in each dimension
+ */
+void dPadMoveSprite(bn::sprite_ptr& sprite, bn::fixed speed);
